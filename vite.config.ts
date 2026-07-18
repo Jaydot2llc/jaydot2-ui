605c87ebcb18d4686d2f1a60d09957cb642e8382
James Bray <jrbrayjr@gmail.com>
2026-07-18 14:24:09 -0500

    revert vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,10 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
-import react from '@vitejs/plugin-react';
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [
-    react(),
     tailwindcss(),
     tsconfigPaths(),
-  ],
-  resolve: {
-    alias: [
-      { 
-        find: './runtimeConfig', 
-        replacement: './runtimeConfig.browser' // ensures browser-compatible AWS JS SDK is used
-      }
-    ]
-  },
-  define: {
-    global: 'window' // polyfill the global object if you see "global is not defined"
-  }
+  ]
 })
